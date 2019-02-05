209623b34c97274fd0557a32b93090c1824ba310
Waga Odongo <wagaodongo@gmail.com>
2019-02-05 08:22:04 +0300

    apollo setup
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import WatchList from './components/WatchkList';
+import WatchList from './components/WatchList';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider } from 'react-apollo'
 
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+})
 
 class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>
       <div id="main">
       <h1>Watch List </h1>
       <WatchList />
       </div>
+      </ApolloProvider>
     );
   }
 }
